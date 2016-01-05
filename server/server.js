e11b2ee01fa17240853ef5bdb43e280af225686a
Steven Law <slaw@nuclearanvil.com>
2016-01-04 17:05:51 -0800

    Added body-parser middleware.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
 var dbLoc = process.env.DB || 'mongodb://localhost/gt3';
 
 mongoose.connect(dbLoc);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client'));
 require('./config/routes.js')(app, express);
 
