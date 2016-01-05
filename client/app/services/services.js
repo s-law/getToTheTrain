04b562462221512c9150633eee19c723fddc6a63
Steven Law <slaw@nuclearanvil.com>
2016-01-04 17:04:57 -0800

    Completed factory component to send position data to server. Note that locating is async.

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,5 +1,5 @@
-angular.module('shortly.services', [])
-.factory('Bart', function ($http) {
+angular.module('gt3.services', [])
+.factory('Bart', function ($http, $q) {
   return {
     getStations: function() {
       return $http({
@@ -9,6 +9,33 @@ angular.module('shortly.services', [])
       .then(function (res) {
         return res.data;
       })
-    };
+    },
+    nearestStation: function() {
+      // promisifies getCurrentPosition()
+      function getPos() {
+        return $q(function(resolve, reject) {
+          navigator.geolocation.getCurrentPosition(function (position) {
+            position = {
+              lat: position.coords.latitude,
+              lon: position.coords.longitude
+            };
+            resolve(position);
+          }, function(err) {
+            reject(err);
+          });
+        })
+      }
+
+      return getPos().then(function(pos) {
+        return $http({
+          method: 'POST',
+          url: '/api/bart-stations',
+          data: pos
+        })
+        .then(function (res) {
+          return res.data;
+        });
+      });
+    }
   }
 });
