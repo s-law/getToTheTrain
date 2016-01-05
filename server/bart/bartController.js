ba52aa69678cc1c2d4ce694952c33e1e001e4174
Steven Law <slaw@nuclearanvil.com>
2016-01-04 17:07:05 -0800

    Takes posted location data and calculates distance to stations.

--- server/bart/bartController.js
+++ server/bart/bartController.js
@@ -1,19 +1,43 @@
 var Bart = require('./bartModel.js');
+var bodyParser = require('body-parser');
 
 module.exports = {
   allStations: function(req, res, next) {
     Bart.find({}, function(err, stations) {
-      console.log(stations);
       var stationList = {};
 
       stations.forEach(function(station) {
-        stationList[station.shortname] = [station.lonDist, station.latDist];
+        stationList[station.shortname] = [station.lon, station.lat];
       });
       res.send(stationList);
     });
   },
 
   nearestStation: function(req, res, next) {
-    return;
+    // optimized Haversine formula:
+    // https://stackoverflow.com/a/21623206
+    var dist = function (lat1, lon1, lat2, lon2) {
+      var p = 0.017453292519943295;    // Math.PI / 180
+      var c = Math.cos;
+      var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+              c(lat1 * p) * c(lat2 * p) * 
+              (1 - c((lon2 - lon1) * p))/2;
+
+      return 7918 * Math.asin(Math.sqrt(a)); // 2 * R; R = 3959 mi
+    }
+
+    var lat = req.body.lat;
+    var lon = req.body.lon;
+
+    Bart.find({}, function(err, stations) {
+      var stationList = [];
+      
+      stations.forEach(function(stn) {
+        stationList.push([stn.shortname, dist(lat, lon, stn.lon, stn.lat)]);
+      });
+
+      console.log(stationList);
+      res.send(stationList);
+    });
   }
 }
