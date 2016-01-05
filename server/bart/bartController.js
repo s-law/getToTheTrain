9f1e18da416fefc1b0101e813705e713738e6908
Steven Law <slaw@nuclearanvil.com>
2016-01-04 19:29:59 -0800

    Processing parsed XML into usable format.

--- server/bart/bartController.js
+++ server/bart/bartController.js
@@ -16,6 +16,7 @@ module.exports = {
   nearestStation: function(req, res, next) {
     var lat = req.body.lat;
     var lon = req.body.lon;
+    var bartKey = process.env.BARTKEY || 'MW9S-E7SL-26DU-VV8V';
 
     Bart.find({}, function(err, stations) {
       var closest = stations.map(function(stn) {
@@ -25,8 +26,22 @@ module.exports = {
         return a[1] < b[1] ? a : b;
       });
 
-      console.log(closest);
-      res.send(closest);
+      utils.bartParse('http://api.bart.gov/api/etd.aspx?cmd=etd&orig=' + closest[0] + '&key=' + bartKey, function(apiRes) {
+        var destEtdObjs = apiRes['root']['station'][0]['etd'];
+
+        var destinations = destEtdObjs.map(function(destEtdObj) {
+          var destName = destEtdObj.destination[0];
+          var departTimes = destEtdObj.estimate.map(function(anEtd) {
+            return parseInt(anEtd.minutes[0]);
+          });
+
+          var destination = {};
+          destination[destName] = departTimes;
+
+          return destination;
+        });
+        res.send(destinations);
+      });
     });
   }
 }
