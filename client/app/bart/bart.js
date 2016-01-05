837ec2bff52d2b54ff569b205f07563a5bd8fa1a
Steven Law <slaw@nuclearanvil.com>
2016-01-04 19:42:42 -0800

    Removed verification code.

--- client/app/bart/bart.js
+++ client/app/bart/bart.js
@@ -2,7 +2,6 @@ angular.module('gt3.bart', [])
 .controller('BartController', function($scope, Bart) {
   Bart.nearestStation()
   .then(function(data) {
-    console.log(data);
     $scope.destinations = data;
   });
 })
