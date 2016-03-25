27e2a3af436b65f61171fc45ae58b789e27706fd
Steven Law <slaw@nuclearanvil.com>
2016-03-24 19:41:03 -0700

    Added API key reference for Caltrain.

--- server/config/apiKeys.js
+++ server/config/apiKeys.js
@@ -1,3 +1,8 @@
+// BART API key shown here is default public key available
+// to anyone without needing to register. 511 does not
+// have a public key available
+
 module.exports = {
-  bart: process.env.BARTKEY || 'MW9S-E7SL-26DU-VV8V'
+  bart: process.env.BARTKEY || 'MW9S-E7SL-26DU-VV8V',
+  caltrain: process.env.CALTRAINKEY
 };
