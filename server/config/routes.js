14e0b3787fc9a96bf581d83682b27378ca357da3
Steven Law <slaw@nuclearanvil.com>
2016-03-24 19:41:48 -0700

    Added additional routes to support requests concerning Caltrain.

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,4 +3,6 @@ var bartController = require('../bart/bartController.js');
 module.exports = function(app, express) {
   app.get('/api/bart-stations', bartController.allStations);
   app.post('/api/bart-stations', bartController.nearestStation);
+  app.get('/api/caltrain-stations', caltrainController.allStation);
+  app.post('/api/caltrain-stations', caltrainController.nearestStation);
 };
