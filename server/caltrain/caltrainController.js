e764390d4bf59409cda6e71fb49a0095751daf50
Steven Law <slaw@nuclearanvil.com>
2016-03-27 22:02:43 -0700

    Nearest station's info now returned.

--- server/caltrain/caltrainController.js
+++ server/caltrain/caltrainController.js
@@ -19,6 +19,18 @@ module.exports = {
   },
 
   nearestStation: function(req, res, next) {
-    res.status(200).send('Caltrain departure information coming soon!');
+    var lat = req.body.lat;
+    var lon = req.body.lon;
+
+    Caltrain.find({}, function(err, stations) {
+      var closest = stations.map(function(stn) {
+        return [stn.station, utils.calcDistance(lat, lon, stn.lat, stn.lon), stn.stopCodeNorth, stn.stopCodeSouth];
+      })
+      .reduce(function(a, b) {
+        return a[1] < b[1] ? a : b;
+      });
+
+      res.send(closest);
+    });
   }
 }
