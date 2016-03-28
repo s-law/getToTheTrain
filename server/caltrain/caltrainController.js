379bda07170f96d3f2b9d5b55e7c6f2a0138f34d
Steven Law <slaw@nuclearanvil.com>
2016-03-27 21:24:20 -0700

    Added API to provide station location and 511 stop codes.

--- server/caltrain/caltrainController.js
+++ server/caltrain/caltrainController.js
@@ -4,7 +4,18 @@ var caltrainApiKey = require('../config/apiKeys.js').caltrain;
 
 module.exports = {
   allStations: function(req, res, next) {
-    res.status(200).send('Caltrain station information coming soon!');
+    Caltrain.find({}, function(err, stations) {
+      var stationList = {};
+
+      stations.forEach(function(station) {
+        stationList[station.station] = {
+          stopCodeNorth: station.stopCodeNorth,
+          stopCodeSouth: station.stopCodeSouth,
+          location: [station.lon, station.lat]
+        }
+      });
+      res.send(stationList);
+    })
   },
 
   nearestStation: function(req, res, next) {
