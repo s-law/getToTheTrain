ccf18cf32eb0cb87ed57a4ea909cb22159e19846
Steven Law <slaw@nuclearanvil.com>
2016-03-27 21:36:10 -0700

    Added reference to Caltrain controller.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,9 @@
 var bartController = require('../bart/bartController.js');
+var caltrainController = require('../caltrain/caltrainController.js');
 
 module.exports = function(app, express) {
   app.get('/api/bart-stations', bartController.allStations);
   app.post('/api/bart-stations', bartController.nearestStation);
-  app.get('/api/caltrain-stations', caltrainController.allStation);
+  app.get('/api/caltrain-stations', caltrainController.allStations);
   app.post('/api/caltrain-stations', caltrainController.nearestStation);
 };
