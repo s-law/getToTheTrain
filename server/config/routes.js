1297040d71b0fc1a07384060acf6b58b719c787f
Steven Law <slaw@nuclearanvil.com>
2018-07-30 00:44:31 -0700

    Add new route to provide information about supported transit systems.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,11 @@
 var bartController = require('../bart/bartController.js');
 var caltrainController = require('../caltrain/caltrainController.js');
+let generalController = require('../general/generalController.js');
 
 module.exports = function(app, express) {
   app.get('/api/bart-stations', bartController.allStations);
   app.post('/api/bart-stations', bartController.nearestStation);
   app.get('/api/caltrain-stations', caltrainController.allStations);
   app.post('/api/caltrain-stations', caltrainController.nearestStation);
+  app.get('/api/v2/general/systems', generalController.supportedTransitSystems);
 };
