4333ce2564e51c4c88583b3f080368902a31b109
Steven Law <slaw@nuclearanvil.com>
2018-08-20 00:41:49 -0700

    Add new routes for general information.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,11 +1,16 @@
 var bartController = require('../bart/bartController.js');
 var caltrainController = require('../caltrain/caltrainController.js');
 let generalController = require('../general/generalController.js');
+const station = require('../station/controller.js')
 
 module.exports = function(app, express) {
-  app.get('/api/bart-stations', bartController.allStations);
-  app.post('/api/bart-stations', bartController.nearestStation);
-  app.get('/api/caltrain-stations', caltrainController.allStations);
-  app.post('/api/caltrain-stations', caltrainController.nearestStation);
-  app.get('/api/v2/general/systems', generalController.supportedTransitSystems);
+    app.get('/api/bart-stations', bartController.allStations);
+    app.post('/api/bart-stations', bartController.nearestStation);
+    app.get('/api/caltrain-stations', caltrainController.allStations);
+    app.post('/api/caltrain-stations', caltrainController.nearestStation);
+    app.get('/api/v2/general/info', generalController.info);
+    app.get('/api/v2/general/systems', generalController.supportedTransitSystems);
+    app.post('/api/v2/station/nearest', station.nearest);
+    app.post('/api/v2/station/nearest/departures', station.nearestNextDepartures);
+    app.post('/api/v2/station/departures', station.departures);
 };
