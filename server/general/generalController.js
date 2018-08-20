e8ca608ac2853409528f4fc19eea1bb08af76cd9
Steven Law <slaw@nuclearanvil.com>
2018-08-20 00:42:57 -0700

    General info.

--- server/general/generalController.js
+++ server/general/generalController.js
@@ -1,9 +1,21 @@
 var utils = require('../config/utils.js');
 
 module.exports = {
-    supportedTransitSystems: function(req, res, next) {
-        let systems = ['bart', 'caltrain', 'smart'];
+    info: function(req, res, next) {
+        const info = {
+            updateRequired: false,
+            showMessage: false,
+            version: 1,
+            message: "Hi!"
+        }
 
-        res.send(systems);
+        res.send(info);
+    },
+    supportedTransitSystems: function(req, res, next) {
+        const clientType = req.query.client;
+        const supportedSystems = clientType && utils.supportedSystems.hasOwnProperty(clientType) ?
+            utils.supportedSystems[clientType] : utils.supportedSystems.default;
+
+        res.send(supportedSystems);
     }
 }
